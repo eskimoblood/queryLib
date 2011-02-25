232b42cb9e08f8a735eea83a532e4b6ff913a3cf
Andreas Köberle <akoeberle@vz.net>
2011-02-25 23:16:54 +0100

    improve geo.places queries

--- src/test/JacksonTest.java
+++ src/test/JacksonTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.logging.Logger;
 
+import yql.geo.placefinder.pojo.PlaceFinder;
+import yql.geo.placefinder.pojo.QueryPlaceFinder;
 import yql.geo.places.QueryPlace;
 import yql.geo.places.pojos.Place;
 import yql.geo.places.pojos.PlaceTypes;
@@ -13,8 +15,9 @@ public class JacksonTest {
 
 	public static void main(String[] args) {
 
-		List<Place> a = QueryPlace.getPlaceBelongtos(12521721);
-		Logger.getLogger("result").info(a.get(0).boundingBox.northEast.latitude + "");
+//		List<Place> a = QueryPlace.getPlaceBelongtos(12521721);
+		PlaceFinder p = QueryPlaceFinder.queryByText(" moabit");
+		Logger.getLogger("result").info(p.city + "");
 
 	}
 }
--- src/yql/geo/places/QueryPlace.java
+++ src/yql/geo/places/QueryPlace.java
@@ -25,14 +25,14 @@ public class QueryPlace {
 
 	/**
 	 * Query against the geo.places table
-	 * 
+	 *
 	 * <p>
 	 * <a href=
 	 * "http://developer.yahoo.com/yql/console/?q=select%20*%20from%20ukpostcode.latlng%20where%20lat%3D'51.5181'%20and%20lon%3D'-0.127115'%20and%20format%3D'xml'%3B#h=select%20*%20from%20geo.places%20where%20text%3D%22sfo%22"
 	 * >Example</a>
-	 * 
+	 *
 	 * @param place
-	 * 
+	 *
 	 * @return
 	 */
 	public static List<Place> getPlace(String place) {
@@ -46,76 +46,89 @@ public class QueryPlace {
 	}
 
 	public static List<Place> getPlace(String place, Locale focus) {
-		String query = "text=\"" + place + "\" and focus=\"" + focus.getCountry() + "\"";
+		String query = "text=\"" + place + "\" and focus=\""
+				+ focus.getCountry() + "\"";
 		return queryPlaceList(query);
 	}
 
 	public static List<Place> getPlace(int woeid, Locale focus) {
-		String query = "text=\"" + woeid + "\" and focus=\"" + focus.getCountry() + "\"";
+		String query = "text=\"" + woeid + "\" and focus=\""
+				+ focus.getCountry() + "\"";
 		return queryPlaceList(query);
 	}
 
 	public static List<Place> getPlace(String place, PlaceTypes placetype) {
-		String query = "text=\"" + place + "\" and placetype=\"" + placetype + "\"";
+		String query = "text=\"" + place + "\" and placetype=\"" + placetype
+				+ "\"";
 		return queryPlaceList(query);
 	}
 
 	public static List<Place> getPlace(int woeid, PlaceTypes placetype) {
-		String query = "text=\"" + woeid + "\" and placetype=\"" + placetype + "\"";
+		String query = "text=\"" + woeid + "\" and placetype=\"" + placetype
+				+ "\"";
 		return queryPlaceList(query);
 	}
 
-	public static List<Place> getPlace(String place, PlaceTypes placetype, Locale focus) {
-		String query = "text=\"" + place + "\" and placetype=\"" + placetype + "\" and focus=\"" + focus.getCountry() + "\"";
+	public static List<Place> getPlace(String place, PlaceTypes placetype,
+			Locale focus) {
+		String query = "text=\"" + place + "\" and placetype=\"" + placetype
+				+ "\" and focus=\"" + focus.getCountry() + "\"";
 		return queryPlaceList(query);
 	}
 
-	public static List<Place> getPlace(int woeid, PlaceTypes placetype, Locale focus) {
-		String query = "text=\"" + woeid + "\" and placetype=\"" + placetype + "\" and focus=\"" + focus.getCountry() + "\"";
+	public static List<Place> getPlace(int woeid, PlaceTypes placetype,
+			Locale focus) {
+		String query = "text=\"" + woeid + "\" and placetype=\"" + placetype
+				+ "\" and focus=\"" + focus.getCountry() + "\"";
 		return queryPlaceList(query);
 	}
 
 	/**
 	 * Query against the geo.places.parent table
-	 * 
+	 *
 	 * @param woeid
 	 * @return
 	 */
 	public static Place getPlaceParent(int woeid) {
-		return queryPlace(Table.GEO_PLACE_PARENT, "child_woeid=\"" + woeid + "\"");
+		return queryPlace(Table.GEO_PLACE_PARENT, "child_woeid=\"" + woeid
+				+ "\"");
 	}
 
 	/**
 	 * Query against the geo.places.belongtos table
-	 * 
+	 *
 	 * @param woeid
 	 * @return
 	 */
 	public static List<Place> getPlaceBelongtos(int woeid) {
-		return queryPlaceList(Table.GEO_PLACES_BELONGSTOS, "member_woeid=\"" + woeid + "\"");
+		return queryPlaceList(Table.GEO_PLACES_BELONGSTOS, "member_woeid=\""
+				+ woeid + "\"");
 	}
 
 	public static List<Place> getPlaceBelongtos(int woeid, PlaceTypes place) {
-		return queryPlaceList(Table.GEO_PLACES_BELONGSTOS, "member_woeid=\"" + woeid + "\" and placetype=\"" + place + "\"");
+		return queryPlaceList(Table.GEO_PLACES_BELONGSTOS, "member_woeid=\""
+				+ woeid + "\" and placetype=\"" + place + "\"");
 	}
 
 	/**
-	 * Query against the geo.places.belongtos table
-	 * 
+	 * Query against the geo.places.children table
+	 *
 	 * @param woeid
 	 * @return
 	 */
 	public static List<Place> getPlaceChildren(int woeid) {
-		return queryPlaceList(Table.GEO_PLACES_CHILDREN, "member_woeid=\"" + woeid + "\"");
+		return queryPlaceList(Table.GEO_PLACES_CHILDREN, "member_woeid=\""
+				+ woeid + "\"");
 	}
 
 	public static List<Place> getPlaceChildren(int woeid, PlaceTypes place) {
-		return queryPlaceList(Table.GEO_PLACES_CHILDREN, "member_woeid=\"" + woeid + "\" and placetype=\"" + place + "\"");
+		return queryPlaceList(Table.GEO_PLACES_CHILDREN, "member_woeid=\""
+				+ woeid + "\" and placetype=\"" + place + "\"");
 	}
 
 	/**
-	 * Query against the geo.places.belongtos table
-	 * 
+	 * Query against the geo.places.common table
+	 *
 	 * @param woeid
 	 * @return
 	 */
@@ -123,47 +136,77 @@ public class QueryPlace {
 		int l = woeids.length;
 		StringBuffer sb = new StringBuffer();
 		if (l < 2 || l > 7) {
-			throw new IllegalArgumentException("the query needs 2 woeids min and max 7");
+			throw new IllegalArgumentException(
+					"the query needs 2 woeids min and max 7");
 		}
 		for (int i = 0; i < woeids.length; i++) {
 			sb.append(" woeid" + i + "=\"");
 			sb.append(woeids[i] + "\"");
-			if(i<l-1){
+			if (i < l - 1) {
 				sb.append(" and ");
 			}
 
 		}
 
-		return queryPlaceList(Table.GEO_PLACES_COMMON, "member_woeid=\"" + sb + "\"");
+		return queryPlaceList(Table.GEO_PLACES_COMMON, "member_woeid=\"" + sb
+				+ "\"");
 	}
 
 	public static List<Place> getPlaceCommon(int woeid, PlaceTypes place) {
-		return queryPlaceList(Table.GEO_PLACES_COMMON, "member_woeid=\"" + woeid + "\" and placetype=\"" + place + "\"");
+		return queryPlaceList(Table.GEO_PLACES_COMMON, "member_woeid=\""
+				+ woeid + "\" and placetype=\"" + place + "\"");
 	}
 
 	/**
 	 * Query against the geo.places.ancestors table
-	 * 
+	 *
 	 * @param woeid
 	 * @return
 	 */
 	public static List<Place> getPlaceAncestors(int woeid) {
-		return queryPlaceList(Table.GEO_PLACES_ANCESTORS, "descendant_woeid=\"" + woeid + "\"");
+		return queryPlaceList(Table.GEO_PLACES_ANCESTORS, "descendant_woeid=\""
+				+ woeid + "\"");
+	}
+
+	/**
+	 * Query against the geo.places.n table
+	 *
+	 * @param woeid
+	 * @return
+	 */
+	public static List<Place> getPlaceNeighbor(int woeid) {
+		return queryPlaceList(Table.GEO_PLACES_NEIGHBORS, "neighbor_woeid=\""
+				+ woeid + "\"");
+	}
+
+	/**
+	 * Query against the geo.places.ancestors table
+	 *
+	 * @param woeid
+	 * @return
+	 */
+	public static List<Place> getPlaceSibling(int woeid) {
+		return queryPlaceList(Table.GEO_PLACES_SIBLING, "sibling_woeid=\""
+				+ woeid + "\"");
 	}
 
 	private static List<Place> queryPlaceList(String query) {
 		return queryPlaceList(Table.GEO_PLACE, query);
 	}
 
+	// TODO create a generic query after the bug is fixed in jackson:
+	// http://jira.codehaus.org/browse/JACKSON-479?page=com.atlassian.jira.plugin.system.issuetabpanels%3Aall-tabpanel
 	private static List<Place> queryPlaceList(Table table, String queryString) {
 		Query query = new Query(table, queryString);
 		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.configure(
+				DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
 		Response<PlaceListResult> r = null;
 		try {
-			r = mapper.readValue(new URL(query.toURL()), new TypeReference<Response<PlaceListResult>>() {
-			});
+			r = mapper.readValue(new URL(query.toURL()),
+					new TypeReference<Response<PlaceListResult>>() {
+					});
 		} catch (JsonParseException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
@@ -184,12 +227,14 @@ public class QueryPlace {
 	private static Place queryPlace(Table table, String queryString) {
 		Query query = new Query(table, queryString);
 		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.configure(
+				DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
 		Response<PlaceResult> r = null;
 		try {
-			r = mapper.readValue(new URL(query.toURL()), new TypeReference<Response<PlaceResult>>() {
-			});
+			r = mapper.readValue(new URL(query.toURL()),
+					new TypeReference<Response<PlaceResult>>() {
+					});
 		} catch (JsonParseException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
